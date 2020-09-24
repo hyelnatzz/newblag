55ee5e56b70696e81383ed54891fd887f457e6db
natzz <hyelnatzz@gmail.com>
2020-09-24 08:50:25 +0100

    basic working models

---
+++ models.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    """Model for user accounts."""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    username = db.Column(db.String(20),
+                         nullable=False,
+                         unique=True)
+    email = db.Column(db.String(40),
+                      unique=True,
+                      nullable=False)
+    full_name = db.Column(db.String(100),
+                          unique=True,
+                          nullable=False)
+    password = db.Column(db.String(200),
+                         unique=False,
+                         nullable=False)
+    posts = db.relationship('Post', backref=db.backref('author'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model, UserMixin):
+    """Model for managing post."""
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    title = db.Column(db.String(200),
+                      nullable=False,
+                      unique=False)
+    subtitle = db.Column(db.String(200),
+                         unique=False,
+                         nullable=False)
+    body = db.Column(db.Text,
+                     unique=False,
+                     nullable=False)
+    author_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        'categories.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.id)
+
+
+class Category(db.Model, UserMixin):
+    """Model for categories of post."""
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    name = db.Column(db.String,
+                     nullable=False,
+                     unique=True)
+    description = db.Column(db.Text,
+                            unique=False,
+                            nullable=False)
+    members = db.relationship('Post', backref=db.backref('category'))
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.name)
