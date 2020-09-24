a3bbe9a43580c7884c10e4ca74f3598611a9d884
natzz <hyelnatzz@gmail.com>
2020-09-24 16:03:35 +0100

    added homepage and created empty templates

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask,render_template,request
 
 
-app=Flask(__name__)
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
 
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method=='POST':
-        return render_template('index.html')
+@app.route('/', methods=['GET','POST'])
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run()
